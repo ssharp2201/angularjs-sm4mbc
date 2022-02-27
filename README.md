af49ceccbff0e9db5baa88768620e49a703bc787
ssharp2201 <ssharp@google.com>
2022-02-27 14:41:25 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angularjs-3pt3bf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angularjs-3pt3bf)
---
+++ home/home.controller.js
@@ -0,0 +1,9 @@
+class HomeCtrl {
+  constructor($scope) {
+    'ngInject';
+
+    this.name = "TDD Workflow";
+  }
+}
+
+export default HomeCtrl;
